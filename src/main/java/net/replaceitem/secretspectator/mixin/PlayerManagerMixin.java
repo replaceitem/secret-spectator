806ae2afa4c4a0da3363e4245dea18e159a7e152
replaceitem <40722305+replaceitem@users.noreply.github.com>
2024-10-27 16:14:03 +0100

    Improved mixins to no longer use redirect

--- src/main/java/net/replaceitem/secretspectator/mixin/PlayerManagerMixin.java
+++ src/main/java/net/replaceitem/secretspectator/mixin/PlayerManagerMixin.java
@@ -1,5 +1,7 @@
 package net.replaceitem.secretspectator.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
 import net.minecraft.server.PlayerManager;
@@ -9,33 +11,32 @@ import net.minecraft.world.GameMode;
 import net.replaceitem.secretspectator.SecretSpectator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
-    @Redirect(method = "onPlayerConnect", at = @At(value = "INVOKE", ordinal = 5, target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V"))
-    private void fakeSelfConnectPacket(ServerPlayNetworkHandler serverPlayNetworkHandler, Packet<?> packet) {
-        ServerPlayerEntity player = serverPlayNetworkHandler.getPlayer();
+    @WrapOperation(method = "onPlayerConnect", at = @At(value = "INVOKE", ordinal = 5, target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V"))
+    private void fakeSelfConnectPacket(ServerPlayNetworkHandler instance, Packet<?> packet, Operation<Void> original) {
+        ServerPlayerEntity player = instance.getPlayer();
         PlayerListS2CPacket playerListS2CPacket = ((PlayerListS2CPacket) packet);
         if(!SecretSpectator.canSeeOtherSpectators(player)) {
             playerListS2CPacket = SecretSpectator.copyPacketWithModifiedEntries(playerListS2CPacket, entry -> SecretSpectator.cloneEntryWithGamemode(entry, GameMode.SURVIVAL));
         }
-        serverPlayNetworkHandler.sendPacket(playerListS2CPacket);
+        original.call(instance, playerListS2CPacket);
     }
 
-    @Redirect(method = "onPlayerConnect", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/server/PlayerManager;sendToAll(Lnet/minecraft/network/packet/Packet;)V"))
-    private void fakeOtherConnectPacket(PlayerManager playerManager, Packet<?> packet) {
+    @WrapOperation(method = "onPlayerConnect", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/server/PlayerManager;sendToAll(Lnet/minecraft/network/packet/Packet;)V"))
+    private void fakeOtherConnectPacket(PlayerManager instance, Packet<?> packet, Operation<Void> original) {
         PlayerListS2CPacket playerListS2CPacket = (PlayerListS2CPacket) packet;
-        PlayerListS2CPacket.Entry entry = playerListS2CPacket.getEntries().get(0);
-        ServerPlayerEntity player = playerManager.getPlayer(entry.profileId());
+        PlayerListS2CPacket.Entry entry = playerListS2CPacket.getEntries().getFirst();
+        ServerPlayerEntity player = instance.getPlayer(entry.profileId());
         if(player != null && player.isSpectator()) {
             // we log in as spectator
             PlayerListS2CPacket fakePacket = SecretSpectator.copyPacketWithModifiedEntries(playerListS2CPacket, entry1 -> SecretSpectator.cloneEntryWithGamemode(entry1, GameMode.SURVIVAL));
-            for (ServerPlayerEntity serverPlayerEntity : playerManager.getPlayerList()) {
+            for (ServerPlayerEntity serverPlayerEntity : instance.getPlayerList()) {
                 serverPlayerEntity.networkHandler.sendPacket(SecretSpectator.canPlayerSeeSpectatorOf(serverPlayerEntity, player) ? packet : fakePacket);
             }
         } else {
-            playerManager.sendToAll(packet);
+            original.call(instance, packet);
         }
     }
 }
--- src/main/java/net/replaceitem/secretspectator/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/net/replaceitem/secretspectator/mixin/ServerPlayerInteractionManagerMixin.java
@@ -1,6 +1,7 @@
 package net.replaceitem.secretspectator.mixin;
 
-import net.minecraft.network.listener.ClientPlayPacketListener;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
 import net.minecraft.server.PlayerManager;
@@ -12,16 +13,15 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerPlayerInteractionManager.class)
 public class ServerPlayerInteractionManagerMixin {
     @Shadow @Final protected ServerPlayerEntity player;
 
-    @Redirect(method = "changeGameMode", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;sendToAll(Lnet/minecraft/network/packet/Packet;)V"))
-    private void sendPackets(PlayerManager playerManager, Packet<ClientPlayPacketListener> packet) {
+    @WrapOperation(method = "changeGameMode", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;sendToAll(Lnet/minecraft/network/packet/Packet;)V"))
+    private void sendPackets(PlayerManager instance, Packet<?> packet, Operation<Void> original) {
         if(this.player.isSpectator()) {
-            for (ServerPlayerEntity serverPlayerEntity : playerManager.getPlayerList()) {
+            for (ServerPlayerEntity serverPlayerEntity : instance.getPlayerList()) {
                 if(SecretSpectator.canPlayerSeeSpectatorOf(serverPlayerEntity, this.player)) {
                     // let other players know who should see us being in spectator
                     serverPlayerEntity.networkHandler.sendPacket(packet);
@@ -33,10 +33,10 @@ public class ServerPlayerInteractionManagerMixin {
             }
         } else {
             // we let all know
-            this.player.server.getPlayerManager().sendToAll(packet);
+            original.call(this.player.server.getPlayerManager(), packet);
             // other spectators tell us they're in survival
             if(!SecretSpectator.canSeeOtherSpectators(this.player)) {
-                for (ServerPlayerEntity serverPlayerEntity : playerManager.getPlayerList()) {
+                for (ServerPlayerEntity serverPlayerEntity : instance.getPlayerList()) {
                     if (this.player != serverPlayerEntity && serverPlayerEntity.isSpectator()) {
                         PlayerListS2CPacket backToSurvivalPacket = SecretSpectator.copyPacketWithModifiedEntries(
                                 new PlayerListS2CPacket(PlayerListS2CPacket.Action.UPDATE_GAME_MODE, serverPlayerEntity),
